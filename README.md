fe5521ab61816e863f3000c95dce54df37b75eae
schachte <rschacht@asu.edu>
2017-03-06 11:12:24 -0700

    added setup files

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 ##Airbnb Data Visualization Backend
+###Dependency Management
+- Installing virtual environment to manage local dependencies `pip install virtualenv'
+- Make a virtualenv in your directory by typing `virtualenv venv`
+- To activate the virtualenv, type in `source venv/bin/activate`
+- Install all the requirements from the requirements.txt file from `pip install -r requirements.txt`
+
+###Setting Up Django Related Elements
+- First, sync the database `python manage.py migrate` (Make sure you're in the directory that manage.py is contained in)
+- Create a superuser that you can test admin priveleges on `python manage.py createsuperuser`
+
+###Running the Server
+`python manage.py runserver`
+
+- 
---
+++ dv_backend/backend/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from django.http import HttpResponse, HttpResponseNotFound
+from backend.serializers import UserSerializer, GroupSerializer
+from django.http import JsonResponse
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+def FatimaIsDingus(request):
+    """
+    This is a test view to show how URLS are mapped to functions for doing the data processing
+    """
+    
+    data = 2+2
+    print(data)
+    return JsonResponse({'sons_name':'kson'})
