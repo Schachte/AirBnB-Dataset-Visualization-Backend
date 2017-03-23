ddc95954fbbe256bd850d15af1c3379a283973a4
schachte <rschacht@asu.edu>
2017-03-22 20:11:12 -0700

    refactor

--- dv_backend/amenities/views.py
+++ dv_backend/amenities/views.py
@@ -19,7 +19,6 @@ def AmenityData(request):
     '''
     
     if (request.method == 'POST'):
-        
         #List requested input params
         req_params = ['city_name', 'metric', 'filters']
         
@@ -47,24 +46,55 @@ def AmenityData(request):
         if not all(params in post_dat for params in req_params):
             return HttpResponse("Missing Parameters", status=422)
         
-        #Parse dict.
-        city_name   = post_dat['city_name']
-        metric      = post_dat['metric']
-        filters = [x.strip(' ') for x in post_dat['filters'].split(',')]
-        filters = ', '.join(filters)
+        #Retrieve clenased information
+        city_name, metric, filters = cleanse_input(post_dat)
         
-        #Build the filter query to only set values to true if that filter was selected
-        filter_query = ''
-        
-        if (filters[0]!=''):
-            for amenity in filters:
-                filter_query += 'and %s = "t" '%(amenity)
-
         #Initialize the query that will get the pricing information based on the input information from the user into the SQL query
         cursor = connection.cursor()
-        if (filter_query != ''):
-            
-            cursor.execute('''
+        cursor.execute(retrieve_query(filters, city_name, amenities))
+        rows = cursor.fetchall()
+
+        #Store return data from the SQL query
+        result = []
+        
+        #Column values in the summary table
+        keys = ('percentDifference', 'averageWithCriteria', 'averageWithoutCriteria', 'totalAverage', 'neighborhood')
+        
+        for row in rows:
+            result.append(dict(zip(keys,row)))
+
+        #Get the average pricing information based on filter selection
+        return HttpResponse(json.dumps(result, indent=4, sort_keys=True, default=str), content_type="application/json", status=200)
+    else:
+        return HttpResponse("Get Req. Unsupported on Amenities", status=405)
+        
+def cleanse_input(post_dat):
+    '''
+    @Description:
+    Clean the input data to process the SQL query
+    '''
+    city_name   = post_dat['city_name']
+    metric      = post_dat['metric']
+    filters = [x.strip(' ') for x in post_dat['filters'].split(',')]
+    filters = ', '.join(filters)
+    
+    return city_name, metric, filters
+        
+        
+def retrieve_query(filters, city, amenities):
+    '''
+    @Description:
+    Retrieve SQL Query
+    '''
+    query_params = ''
+    
+    #null params 
+    if (not filters):
+        query_params = ','.join(amenities)
+    else:
+        query_params = filters
+        
+    return ('''
 SELECT
     ( ( avgWithCriteria - totalAverage ) / ( ( avgWithCriteria + totalAverage ) / 2 ) ) * 100 as percentDifference,
     a.*
@@ -77,36 +107,6 @@ FROM
     FROM listings 
     WHERE city_name = '%s'
     GROUP BY neighbourhood_cleansed ) a; 
-            '''%(filters, filters, city_name))
-        else:
-            cursor.execute('''
-SELECT
-    ( ( avgWithCriteria - totalAverage ) / ( ( avgWithCriteria + totalAverage ) / 2 ) ) * 100 as percentDifference,
-    a.*
-FROM
-    (SELECT
-        AVG( CASE WHEN 'f' not in ( host_is_superhost, instant_bookable, has_internet, has_kitchen, allows_pets, has_free_parking, has_pool, has_gym, has_tv, pets_live_on_property, allows_smoking,has_washer_and_dryer) THEN price ELSE null END) as avgWithCriteria,
-        AVG( CASE WHEN 'f'        in ( host_is_superhost, instant_bookable, has_internet, has_kitchen, allows_pets, has_free_parking, has_pool, has_gym, has_tv, pets_live_on_property, allows_smoking,has_washer_and_dryer ) THEN price ELSE null END) as avgWithoutCriteria,
-        AVG( price ) as totalAverage,
-        neighbourhood_cleansed
-    FROM listings 
-    WHERE city_name = 'Brussels'
-    GROUP BY neighbourhood_cleansed ) a;            
-            ''')
-        rows = cursor.fetchall()
-
-        #Store return data from the SQL query
-        result = []
+'''%(query_params, query_params, city))
         
-        #Column values in the summary table
-        keys = ('percentDifference', 'averageWithCriteria', 'averageWithoutCriteria', 'totalAverage', 'neighborhood')
-        
-        for row in rows:
-            result.append(dict(zip(keys,row)))
-        
-        json_data = json.dumps(result, indent=4, sort_keys=True, default=str)
-
-        #Get the average pricing information based on filter selection
-        return HttpResponse(json_data, content_type="application/json", status=200)
-    else:
-        return HttpResponse("Get Req. Unsupported on Amenities", status=405)
+    
